1c652749abcde96dd0ced84d97929a65160677a9
Arturo <adquirose@gmail.com>
2026-02-04 15:53:38 -0300

    Configure proxy for external PDF to solve CORS issues

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,24 @@ export default defineConfig({
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+    proxy: {
+      '/api/pdf': {
+        target: 'https://www.lanube360.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/pdf/, '/pdf'),
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
     }
   }
 })
